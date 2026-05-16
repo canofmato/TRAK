562f49a4d706f4de91cf319f78a6d2beb79db1eb
qkrekdls <gunmannduu@gmail.com>
2026-05-16 15:40:47 +0900

    add:folder svg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ webpack(config) {
+    // 기존 Next.js SVG 처리 룰 제거
+    const fileLoaderRule = config.module.rules.find((rule: { test?: RegExp }) =>
+      rule.test?.test?.(".svg")
+    );
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/;
+    }
+
+    // SVGR로 SVG → React 컴포넌트
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    
+    return config;
+  },
 };
 
 export default nextConfig;
