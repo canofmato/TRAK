82666a5adc4d972ceeed1c6c0862dabe68d75fb6
qkrekdls <gunmannduu@gmail.com>
2026-05-20 17:04:08 +0900

    feat: initial page setting

---
+++ src/app/main/page.tsx
@@ -0,0 +1,17 @@
+import { Footer } from "@/components/layout/Footer";
+import { Header } from "@/components/layout/Header";
+
+export default function MainPage() {
+  return (
+    <div className="w-full h-ful flex flex-col items-center justify-center">
+      <Header />
+
+      {/* main */}
+      <div className="w-full h-full pt-[60px]">
+
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
